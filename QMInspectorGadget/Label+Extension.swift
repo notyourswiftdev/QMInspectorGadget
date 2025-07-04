393669776d4ab982f399ba7cde61f2a0b164c2bb
Aaron Cleveland <toscleveland@gmail.com>
2025-07-03 22:17:45 -0500

    removed demo project, just to simplify things and move to just a workspace. added an extension for label just to verify the project is working properly initialized.

---
+++ QMInspectorGadget/Label+Extension.swift
@@ -0,0 +1,13 @@
+import UIKit
+import ObjectiveC
+
+public extension UILabel {
+    @objc func qmSetText(_ text: String) {
+        self.qmSetText(text)
+        print("[INSPECTOR GADGET - LOGGING] - UILabel: \(self) text changed to: \(text)")
+    }
+}
+
+public extension UIViewController {
+    
+}
--- QMInspectorGadget/QMInspectorGadget.swift
+++ QMInspectorGadget/QMInspectorGadget.swift
@@ -1,18 +1,48 @@
-//
-//  QMInspectorGadget.swift
-//  QMInspectorGadget
-//
-//  Created by Aaron Cleveland on 7/2/25.
-//
-
 import Foundation
 import ObjectiveC
 
-public class QMTextChangeLogger {
-    public static let shared = QMTextChangeLogger()
+public class QMInspectorGadget {
+    public static let shared = QMInspectorGadget()
     
     public init() {
         
     }
+    
+    public func loggingScreenName(screenName: String) {
+        print("[INSPECTOR GADGET - LOGGING] - Screen Name: \(screenName)")
+    }
 }
 
+//// QMTextChangeLogger: A framework-style utility for logging UILabel text changes via swizzling
+//import UIKit
+//import ObjectiveC
+//
+//public class QMTextChangeLogger {
+//    public static let shared = QMTextChangeLogger()
+//    private static var didSwizzle = false
+//
+//    private init() {
+//        self.swizzleUILabelTextSetter()
+//    }
+//
+//    private func swizzleUILabelTextSetter() {
+//        guard !Self.didSwizzle else { return }
+//        Self.didSwizzle = true
+//        let cls: AnyClass = UILabel.self
+//        let originalSelector = #selector(setter: UILabel.text)
+//        let swizzledSelector = #selector(UILabel.qm_setText(_:))
+//
+//        guard let originalMethod = class_getInstanceMethod(cls, originalSelector),
+//              let swizzledMethod = class_getInstanceMethod(cls, swizzledSelector) else { return }
+//
+//        method_exchangeImplementations(originalMethod, swizzledMethod)
+//    }
+//}
+//
+//extension UILabel {
+//    @objc func qm_setText(_ text: String?) {
+//        // Call the original setter (now swapped)
+//        self.qm_setText(text)
+//        print("[QMTextChangeLogger] UILabel: \(self) text changed to: \(text ?? "<nil>")")
+//    }
+//}
